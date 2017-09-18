3dddd8f1648ca583eac068acf7958ff1dc83e141
Bartosz Gajda <bgajda@red-paint.com>
2017-09-18 14:54:05 +0100

    Some Eloquent nonsense...

--- database/migrations/2017_09_18_133910_create_tasks_table.php
+++ database/migrations/2017_09_18_133910_create_tasks_table.php
@@ -15,8 +15,8 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->text('body');
+            $table->boolean('isCompleted')->default(false);
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Task;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,13 +22,15 @@ Route::get('/', function () {
 });
 
 Route::get('/tasks', function () {
-    $tasks = DB::table('tasks')->get();
+//    $tasks = DB::table('tasks')->get();
+    $tasks = App\Task::all();
 
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
-    $task = DB::table('tasks')->find($id);
+//    $task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
 
     return view('tasks.show', compact('task'));
 });
